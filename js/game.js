2bd533d88a9e810309e761a3de87f6653051522b
MaximumCarnage <dylansbruton@gmail.com>
2018-04-06 20:37:56 -0400

    adding tiled map, main character sprite, character movement

--- js/game.js
+++ js/game.js
@@ -4,8 +4,42 @@ let gameScene = new Phaser.Scene('Game');
 let config = {
   type: Phaser.Canvas,  
   width: window.innerWidth * window.devicePixelRatio, 
-  height: window.innerHeight * window.devicePixelRatio, 
+  height: window.innerHeight * window.devicePixelRatio,
+   physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 }
+        }
+   }, 
   scene: gameScene 
 };
 
-let game = new Phaser.Game(config);
+var game = new Phaser.Game(config);
+
+gameScene.init = function() {
+	this.playerSpeed = 1.5;
+}
+
+gameScene.preload = function() {
+	this.load.image('background', 'assets/images/Desert.png');
+	this.load.image('player', 'assets/sprites/MainCharacterstatic.png');
+}	
+
+gameScene.create = function() {
+
+	let bg = this.add.sprite(0, 0, 'background');
+	bg.tint = 0x444444;
+  	bg.setOrigin(0,0);
+	this.player = this.physics.add.sprite(30,40 , 'player');
+}
+
+gameScene.update = function() {
+	if (this.input.activePointer.isDown) {
+    	this.physics.moveTo(this.player, this.input.x + this.cameras.main.scrollX, this.input.y + this.cameras.main.scrollY, null, 1000);
+
+  	}
+  	else if(this.input.activePointer.isUp){
+  		this.player.velocity = 0;
+  	}
+}
+
